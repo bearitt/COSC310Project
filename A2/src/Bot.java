5b1135dc7847632219138d6a1a7b6c204f44326c
Barret Jackson <barret.jackson@gmail.com>
2020-03-10 16:45:18 -0700

    Implement getQuestionType method in Bot, sample usage in conversation

--- A2/src/Bot.java
+++ A2/src/Bot.java
@@ -1,19 +1,52 @@
 import java.io.Console;
 
 public class Bot {
+	Console console = System.console();
+	
+	public void receiveQ(String question) {
+		String[] questionSplit = question.split(" ");
+		int type = getQuestionType(question);
+		console.printf("Your question is type %d",type);
+	}
+	
+	public int getQuestionType(String question) {
+		String[] questionSplit = question.split(" ");
+		int type;
+		switch(questionSplit[0].toLowerCase()) {
+		case "where":
+			type=0;
+			break;
+		case "what":
+			type=1;
+			break;
+		case "how":
+			type=2;
+			break;
+		case "when":
+			type=3;
+			break;
+		default:
+			type=-1;
+			break;
+		}
+		return type;
+	}
+	public void sendA() {}
+	public void askQ() {}
+	public void receiveA() {}
 	
 	public void delayResponse() {
-		String[] spinner = new String[] {"\u0008\u0008\u0008.  ","\u0008\u0008\u0008.. ", "\u0008\u0008\u0008..."};
-        Console console = System.console();
-        console.printf("Chatbot: ");
-        console.printf(".  ");
-        try {
-        for (int i = 0; i < 100; i++) {
-            Thread.sleep(150);
-            console.printf("%s", spinner[i % spinner.length]);
-        }
-        } catch(InterruptedException e) {
-        	console.printf("Something went wrong\n");
-        }
+		String[] spinner = new String[] { "\u0008\u0008\u0008.  ", "\u0008\u0008\u0008.. ", "\u0008\u0008\u0008..." };
+		console.printf("Chatbot: ");
+		console.printf(".  ");
+		try {
+			for (int i = 0; i < 30; i++) {
+				Thread.sleep(150);
+				console.printf("%s", spinner[i % spinner.length]);
+			}
+			console.printf("\u0008\u0008\u0008");
+		} catch (InterruptedException e) {
+			console.printf("Something went wrong\n");
+		}
 	}
 }
--- A2/src/Conversation.java
+++ A2/src/Conversation.java
@@ -1,9 +1,17 @@
-
+import java.io.Console;
+import java.util.Scanner;
 public class Conversation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Bot chatbot = new Bot();
+		Console console = System.console();
+		console.printf("Enter your question: ");
 
+		Scanner in = new Scanner(System.in);
+		String question = in.nextLine();
+		chatbot.receiveQ(question);
+		
+		in.close();
 	}
 
 }
