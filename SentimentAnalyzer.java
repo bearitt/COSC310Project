665b508b3312aae69e963c6a4923d3cebdd9a0db
JakeEgarhos <57509315+JakeEgarhos@users.noreply.github.com>
2020-04-04 13:09:33 -0700

    Add function that reads in string and returns sentiment rating

---
+++ SentimentAnalyzer.java
@@ -0,0 +1,33 @@
+
+
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.util.CoreMap;
+
+public class SentimentAnalyzer {
+
+	public int getStanfordSentimentRate(String sentimentText) {
+	    Properties props = new Properties();
+	    props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
+	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+	    //StanfordCoreNLP
+	    int totalRate = 0;
+	    String[] linesArr = sentimentText.split("\\.");
+	    for (int i = 0; i < linesArr.length; i++) {
+	        if (linesArr[i] != null) {
+	            edu.stanford.nlp.pipeline.Annotation annotation = pipeline.process(linesArr[i]);
+	            for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
+	                Tree tree = sentence.get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
+	                int score = RNNCoreAnnotations.getPredictedClass(tree);
+	                totalRate = totalRate + (score - 2);
+	            }
+	        }
+	    }
+	    return totalRate;
+	}
+}
