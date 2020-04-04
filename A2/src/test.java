aced1d7dc767e2c6a5ab8abb204bb1605c1336ea
JakeEgarhos <jakee1354@gmail.com>
2020-04-04 15:56:02 -0700

    Add some stuff

--- A2/src/test.java
+++ A2/src/test.java
@@ -2,9 +2,7 @@ import java.io.IOException;
 
 public class test {
 
-	public static void main(String[] args) throws ClassNotFoundException, IOException {
-		
-		TaggerExample tagging = new TaggerExample();
-        tagging.tag("If you have several test classes, you can combine them into a test suite.");
+	public static void main(String[] args)  {
+		System.out.println(SentimentAnalyzer.getStanfordSentimentRate("henlo"));
 	}
 }
